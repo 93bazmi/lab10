8d37752853e45dd7ca98f1f857b50e139dadceb5
bazmi <bambamboom234@gmail.com>
2023-01-11 18:35:48 +0700

    lab10_1.cpp completed

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,31 +3,53 @@
 using namespace std;
 
 int main(){	
-	cout << "Enter initial loan: ";
-	cout << "Enter interest rate per year (%): ";
-	cout << "Enter amount you can pay per year: ";
+	double first,rate,pay,PrevBalance,Interest,Total,Payment,NewBalance ;
+	int year = 1;
+	cout << "Enter initial loan: " ;
+	cin >> first ;
+	cout << "Enter interest rate per year (%): " ;
+	cin >> rate ;
+	cout << "Enter amount you can pay per year: " ;
+	cin >> pay ;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
-	cout << setw(13) << left << "EndOfYear#"; 
+	cout << setw(13) << left << "EndOfYear#" ; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
 	cout << setw(13) << left << "Total";
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
-	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+
+	while(NewBalance!=0){
+		if(year==1){
+			PrevBalance = first ;
+		}else{
+			PrevBalance = NewBalance ;
+		}
+		Interest = PrevBalance*(rate/100) ;
+		Total = PrevBalance+Interest ;
+		if(pay > Total){
+			Payment = Total ;
+		}else{
+			Payment = pay ;
+		}
+		NewBalance = Total - Payment ;
+
+		//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
+		//you can change input argument of 'setprecision()' to see the effect
+		cout << fixed << setprecision(2); 
+		cout << setw(13) << left << year ; 
+		cout << setw(13) << left << PrevBalance ;
+		cout << setw(13) << left << Interest ;
+		cout << setw(13) << left << Total ;
+		cout << setw(13) << left << Payment ;
+		cout << setw(13) << left << NewBalance ;
+		cout << "\n";	
+		year++ ;
+	}	
 	
 	return 0;
 }
